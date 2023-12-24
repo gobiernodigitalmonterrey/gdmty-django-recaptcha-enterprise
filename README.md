164fea64074d2a7891dc9c688d3170ca3be92887
Niji <mathereall@gmail.com>
2023-12-24 16:48:32 -0600

    actualización de README.md

--- README.md
+++ README.md
@@ -15,6 +15,12 @@ Usage:
 In settings.py:
 
 ```python
+# import service_account from google.oauth2 and instanciate a Credentials object from your service account file 
+
+from google.oauth2 import service_account
+credentials = service_account.Credentials.from_service_account_file("YOUR_RECAPTCHA_CREDENTIALS_SERVICE_ACCOUNT_FILE")
+
+
 # put 'gdmty_django_recaptcha_enterprise' in INSTALLED_APPS
 
 INSTALLED_APPS = [
@@ -23,35 +29,38 @@ INSTALLED_APPS = [
     ...
 ]
 
-
-# import service_account from google.oauth2 and instanciate a Credentials object from your service account file 
-
-from google.oauth2 import service_account
-credentials = service_account.Credentials.from_service_account_file("YOUR_RECAPTCHA_CREDENTIALS_SERVICE_ACCOUNT_FILE")
-
-
 # Set the following variables 
 
-RECAPTCHA_ENTERPRISE_SITE_KEY = 'your-site-key'
 RECAPTCHA_ENTERPRISE_PROJECT_ID = 'your-project-id'
 RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS = credentials
+RECAPTCHA_ENTERPRISE_SITE_KEY = 'your-site-key' # Additonal, maybe you want to use different keys for different actions; in that case you can use many variables like this one, and choose the right one on instanciate.
 RECAPTCHA_ENTERPRISE_BYPASS_TOKEN = 'your-bypass-token' # Optional, only for debug and development usage. Don't use in production.
+
+
 ```
 
-In your view:
+In your code:
 
 ```python
 
+
 # import assess_token from gdmty_django_recaptcha_enterprise.recaptcha, then you can use it to assess tokens where you need it. In this excample we show a hypothetical view that receives a token from a POST request.
 
-from gdmty_django_recaptcha_enterprise.recaptcha import assess_token
+from gdmty_django_recaptcha_enterprise.recaptcha import RecaptchaEnterprise
+from django.conf import settings
+
+
+recaptcha = RecaptchaEnterprise(
+    settings.RECAPTCHA_ENTERPRISE_PROJECT_ID, 
+    settings.RECAPTCHA_ENTERPRISE_SITE_KEY, 
+    settings.RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS)
 
 
 def my_view(request):
     ...
     token = request.POST.get('token')
     action = 'action-to-verify'
-    if assess_token(token, action):
+    if recaptcha.assess_token(token, action):
         # Token is valid
         pass
     else:
--- src/gdmty_django_recaptcha_enterprise/recaptcha.py
+++ src/gdmty_django_recaptcha_enterprise/recaptcha.py
@@ -4,85 +4,88 @@ from django.conf import settings
 from .utils import console_log
 from secrets import compare_digest
 
-
 DEBUG = settings.DEBUG
-RECAPTCHA_ENTERPRISE_SITE_KEY = settings.RECAPTCHA_ENTERPRISE_SITE_KEY
-RECAPTCHA_ENTERPRISE_PROJECT_ID = settings.RECAPTCHA_ENTERPRISE_PROJECT_ID
-RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS = settings.RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS
 RECAPTCHA_ENTERPRISE_BYPASS_TOKEN = settings.RECAPTCHA_ENTERPRISE_BYPASS_TOKEN
 
 
-def create_assessment(project_id: str, recaptcha_site_key: str, token: str, recaptcha_action: str) -> Assessment | None:
-    """Create an assessment to analyze the risk of a UI action.
-    Args:
-        project_id: GCloud Project ID
-        recaptcha_site_key: Site key obtained by registering a domain/app to use recaptcha services.
-        token: The token obtained from the client on passing the recaptchaSiteKey.
-        recaptcha_action: Action name corresponding to the token.
-    """
+class RecaptchaEnterprise:
+    def __init__(self, project_id: str = None, site_key: str = None, credentials: str = None):
+        """Create an assessment to analyze the risk of a UI action.
+        Args:
+            project_id: GCloud Project ID
+            site_key: Site key obtained by registering a domain/app to use recaptcha services.
+            credentials: The token obtained from the client on passing the recaptchaSiteKey.
+        """
+        self.project_id = project_id
+        self.site_key = site_key
+        self.service_account_credentials = credentials
 
-    client = recaptchaenterprise_v1.RecaptchaEnterpriseServiceClient(
-        credentials=RECAPTCHA_ENTERPRISE_SERVICE_ACCOUNT_CREDENTIALS
-    )
+        self.client = recaptchaenterprise_v1.RecaptchaEnterpriseServiceClient(credentials=credentials)
 
-    # Set the properties of the event to be tracked.
-    event = recaptchaenterprise_v1.Event()
-    event.site_key = recaptcha_site_key
-    event.token = token
+    def create_assessment(self, token: str, recaptcha_action: str = None) -> Assessment | None:
+        """Create an assessment to analyze the risk of a UI action.
+        Args:
+            token: The token obtained from the client on passing the recaptchaSiteKey.
+            recaptcha_action: Action name corresponding to the token.
+        """
 
-    assessment = recaptchaenterprise_v1.Assessment()
-    assessment.event = event
+        # Set the properties of the event to be tracked.
+        event = recaptchaenterprise_v1.Event()
+        event.site_key = self.site_key
+        event.token = token
 
-    project_name = f"projects/{project_id}"
+        assessment = recaptchaenterprise_v1.Assessment()
+        assessment.event = event
 
-    # Build the assessment request.
-    request = recaptchaenterprise_v1.CreateAssessmentRequest()
-    request.assessment = assessment
-    request.parent = project_name
+        project_name = f"projects/{self.project_id}"
 
-    response = client.create_assessment(request)
+        # Build the assessment request.
+        request = recaptchaenterprise_v1.CreateAssessmentRequest()
+        request.assessment = assessment
+        request.parent = project_name
 
-    # Check if the token is valid.
-    if not response.token_properties.valid:
-        console_log(
-            "The CreateAssessment call failed because the token was "
-            + "invalid for for the following reasons: "
-            + str(response.token_properties.invalid_reason)
-        )
-        return
+        response = self.client.create_assessment(request)
 
-    # Check if the expected action was executed.
-    if response.token_properties.action != recaptcha_action:
-        console_log(
-            "The action attribute in your reCAPTCHA tag does"
-            + "not match the action you are expecting to score"
-        )
-        return
-    else:
-        # Get the risk score and the reason(s)
-        # For more information on interpreting the assessment,
-        # see: https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
-        for reason in response.risk_analysis.reasons:
-            console_log(reason)
-        console_log(f"The reCAPTCHA score for this token is: {str(response.risk_analysis.score)}")
-        # Get the assessment name (id). Use this to annotate the assessment.
-        assessment_name = client.parse_assessment_path(response.name).get("assessment")
-        console_log(f"Assessment name: {assessment_name}")
-    return response
+        # Check if the token is valid.
+        if not response.token_properties.valid:
+            console_log(
+                "The CreateAssessment call failed because the token was "
+                + "invalid for for the following reasons: "
+                + str(response.token_properties.invalid_reason)
+            )
+            return
 
+        # Check if the expected action was executed.
+        if response.token_properties.action != recaptcha_action:
+            console_log(
+                "The action attribute in your reCAPTCHA tag does"
+                + "not match the action you are expecting to score"
+            )
+            return
+        else:
+            # Get the risk score and the reason(s)
+            # For more information on interpreting the assessment,
+            # see: https://cloud.google.com/recaptcha-enterprise/docs/interpret-assessment
+            for reason in response.risk_analysis.reasons:
+                console_log(reason)
+            console_log(f"The reCAPTCHA score for this token is: {str(response.risk_analysis.score)}")
+            # Get the assessment name (id). Use this to annotate the assessment.
+            assessment_name = self.client.parse_assessment_path(response.name).get("assessment")
+            console_log(f"Assessment name: {assessment_name}")
+        return response
 
-def assess_token(token: str, recaptcha_action: str = None) -> bool:
-    """Create an assessment of a token for a given action.
-    Args:
-        :param token: The token obtained from the client on passing the recaptchaSiteKey.
-        :param recaptcha_action: The action name used to assess the token.
-    """
-    if DEBUG and RECAPTCHA_ENTERPRISE_BYPASS_TOKEN:
-        if compare_digest(token, RECAPTCHA_ENTERPRISE_BYPASS_TOKEN):
-            return True
+    def assess_token(self, token: str, action: str = None) -> bool:
+        """Create an assessment of a token for a given action.
+        Args:
+            :param token: The token obtained from the client on passing the recaptchaSiteKey.
+            :param action: The action name used to assess the token.
+        """
+        if DEBUG and RECAPTCHA_ENTERPRISE_BYPASS_TOKEN:
+            if compare_digest(token, RECAPTCHA_ENTERPRISE_BYPASS_TOKEN):
+                return True
 
-    response = create_assessment(RECAPTCHA_ENTERPRISE_PROJECT_ID, RECAPTCHA_ENTERPRISE_SITE_KEY, token, recaptcha_action)
+        response = self.create_assessment(token, action)
 
-    if response:
-        return response.token_properties.valid
-    return False
+        if response:
+            return response.token_properties.valid
+        return False
