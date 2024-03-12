7397f327b93de1539b84495eecc0a13fb6ec806a
César Benjamín <mathereall@gmail.com>
2024-03-12 11:41:25 -0600

    corrección a el bypass en debug para permitir utilizar el token o la cadena de bypass

--- src/gdmty_django_recaptcha_enterprise/recaptcha.py
+++ src/gdmty_django_recaptcha_enterprise/recaptcha.py
@@ -74,8 +74,11 @@ class RecaptchaEnterprise:
             :param token: The token obtained from the client on passing the recaptchaSiteKey.
             :param action: The action name used to assess the token.
         """
-        if DEBUG and RECAPTCHA_ENTERPRISE_BYPASS_TOKEN is not False and compare_digest(token, RECAPTCHA_ENTERPRISE_BYPASS_TOKEN):
-            return True
+
+        if DEBUG:
+            if RECAPTCHA_ENTERPRISE_BYPASS_TOKEN is not False:
+                if compare_digest(token, RECAPTCHA_ENTERPRISE_BYPASS_TOKEN):
+                    return True
 
         if not DEBUG and RECAPTCHA_ENTERPRISE_BYPASS_TOKEN is not False:
             return False
