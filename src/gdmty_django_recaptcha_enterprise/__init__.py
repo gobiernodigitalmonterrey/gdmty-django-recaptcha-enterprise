156662e5a9338e1a8167a773bad6e2bc7ec6ece4
César Benjamín <mathereall@gmail.com>
2024-03-14 11:12:08 -0600

    bump version 24.3.2-dev1 -> 24.3.2-dev2

--- src/gdmty_django_recaptcha_enterprise/__init__.py
+++ src/gdmty_django_recaptcha_enterprise/__init__.py
@@ -14,7 +14,7 @@ Usage:
 """
 
 __title__ = "gdmty_django_recaptcha_enterprise"
-__version__ = "24.3.2-dev1"
+__version__ = "24.3.2-dev2"
 __description__ = "reCAPTCHA Enterprise's Django module for verifying reCAPTCHA tokens"
 __url__ = "https://github.com/SIGAMty/gdmty-django-recaptcha-enterprise"
 __author__ = "César Benjamín"
